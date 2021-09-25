2aa27b002c515bfc383fa1867f9603f6c0b09816
ceruleanox <ceruleanox@gmail.com>
2021-09-24 23:29:24 -0400

    edited FB to have black background and no images

---
+++ popup.js
@@ -0,0 +1,29 @@
+console.log("in popup.js");
+
+// Initialize butotn with users's prefered color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: facebookToSlack,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function facebookToSlack() {
+  /*
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+  */
+  console.log("button clicked");
+}
